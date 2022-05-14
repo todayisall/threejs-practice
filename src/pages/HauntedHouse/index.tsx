e8243bba413abba94997783b7dc2abdcf25f6190
王鹏飞 <todayisall@qq.com>
2022-05-14 21:44:25 +0800

    feat: 去掉桌子的模型

--- src/pages/HauntedHouse/index.tsx
+++ src/pages/HauntedHouse/index.tsx
@@ -4,17 +4,8 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import * as dat from "dat.gui";
-import TWEEN from "@tweenjs/tween.js";
 
 const HauntedHouse: FunctionComponent = () => {
-  const params = {
-    envMap: "HDR",
-    roughness: 0.0,
-    metalness: 0.0,
-    exposure: 1.0,
-    debug: false,
-  };
-
   let isPlaying = false;
   let deskStatus = "up"; // down
   let posSound: any = null;
@@ -30,11 +21,6 @@ const HauntedHouse: FunctionComponent = () => {
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
 
-    // 第一种方式 6张图片的环境贴图
-    // scene.background = new THREE.CubeTextureLoader()
-    //   .setPath("/textures/cube/indoor/")
-    //   .load(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]);
-
     /**
      * camera
     //  */
@@ -45,79 +31,7 @@ const HauntedHouse: FunctionComponent = () => {
       600
     );
     camera.position.set(0, 30, 130);
-    const listener = new THREE.AudioListener();
-    camera.add(listener);
     scene.add(camera);
-
-    // const helper = new THREE.CameraHelper(camera);
-    // scene.add(helper);
-    // const axesHelper = new THREE.AxesHelper(100);
-    // scene.add(axesHelper);
-
-    const startAnimation = () => {
-      const deskTop = scene.getObjectByName("Table");
-      const deskPositionY = [1.2, 0.75];
-      if (deskTop?.position && !isPlaying) {
-        posSound.play();
-        const gentTween = new TWEEN.Tween(deskTop.position)
-          .to(
-            {
-              y: deskPositionY[deskStatus === "up" ? 0 : 1],
-            },
-            2000
-          )
-          .onComplete((obj) => {
-            isPlaying = false;
-          })
-          .start();
-        isPlaying = true;
-      }
-    };
-
-    const panelControls = new (function () {
-      this.deskStatus = deskStatus;
-    })();
-    const controlsGUI = new dat.GUI({ name: "Controls" });
-    controlsGUI
-      .add(panelControls, "deskStatus", ["up", "down"])
-      .onChange((value) => {
-        deskStatus = value;
-        startAnimation();
-      });
-    scene.add(controlsGUI);
-    /**
-     * 加载桌子
-     */
-    loader.load(
-      // resource URL
-      "/models/desk/lifting_desk.gltf",
-      function (gltf: any) {
-        console.log("gltf", gltf);
-        gltf.scene.scale.set(50, 50, 50);
-        gltf.scene.position.set(0, -50, 0);
-        scene.add(gltf.scene);
-
-        const deskTop = scene.getObjectByName("Table");
-        // 声音文件.
-        if (deskTop?.position) {
-          posSound = new THREE.PositionalAudio(listener);
-          const audioLoader = new THREE.AudioLoader();
-          audioLoader.load("/models/desk/audio.mp3", (buffer) => {
-            posSound.setBuffer(buffer);
-            posSound.setRefDistance(30);
-            // posSound.posSound.setRolloffFactor(0.8);
-          });
-        }
-      },
-      // called while loading is progressing
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-      },
-      // called when loading has errors
-      function (error) {
-        console.log("An error happened");
-      }
-    );
 
     /**
      * renderer
@@ -169,16 +83,11 @@ const HauntedHouse: FunctionComponent = () => {
 
     // animations
     const tick = () => {
-      TWEEN.update();
       requestAnimationFrame(tick);
       controls.update();
       renderer.render(scene, camera);
     };
     tick();
-    return () => {
-      // Clean up the subscription
-      controlsGUI.destroy();
-    };
   }, []);
 
   return <canvas className="webGl"></canvas>;
