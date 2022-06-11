93a48abf521665309637283961ede9938ea5cb75
王鹏飞 <todayisall@qq.com>
2022-06-11 17:50:59 +0800

    feat: 增加物理效果下的背景, 支持弹跳

--- src/pages/Physics/index.tsx
+++ src/pages/Physics/index.tsx
@@ -1,9 +1,10 @@
-import { FunctionComponent, useEffect } from "react";
+import { Fragment, FunctionComponent, useEffect } from "react";
 import { initStats, initWebGl } from "../common";
 import * as THREE from "three";
 import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as CANNON from "cannon-es";
+import { Color } from "three";
 const Physics: FunctionComponent = () => {
   let scene: THREE.Scene;
   let camera: THREE.Camera;
@@ -41,6 +42,7 @@ const Physics: FunctionComponent = () => {
     const geometry = new THREE.SphereGeometry(radius);
     const material = new THREE.MeshStandardMaterial();
     const sphereMesh = new THREE.Mesh(geometry, material);
+    sphereMesh.castShadow = true;
     sphereMesh.position.set(0, 10, 0);
     scene.add(sphereMesh);
     return {
@@ -51,6 +53,7 @@ const Physics: FunctionComponent = () => {
   useEffect(() => {
     const initResult = initWebGl();
     scene = initResult.scene;
+    scene.background = new Color(0x444444);
     camera = initResult.camera;
     camera.position.set(5, 5, 5);
     renderer = initResult.renderer;
@@ -61,6 +64,18 @@ const Physics: FunctionComponent = () => {
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
 
+    const textureLoader = new THREE.TextureLoader();
+    const galaxyTexture = textureLoader.load("/images/galaxy.jpeg");
+
+    scene.add(
+      new THREE.Mesh(
+        new THREE.SphereBufferGeometry(100, 32, 32),
+        new THREE.MeshBasicMaterial({
+          side: THREE.BackSide,
+          map: galaxyTexture,
+        })
+      )
+    );
     // light
     const pointLight = new THREE.PointLight("#ffffff", 1, 10);
     pointLight.position.set(2, 5, -1);
@@ -69,10 +84,26 @@ const Physics: FunctionComponent = () => {
     const ambientLight = new THREE.AmbientLight(0x404040);
     scene.add(ambientLight);
 
+    const planeGeometry = new THREE.PlaneGeometry(30, 30, 18, 18);
+    const lineMaterial = new THREE.LineBasicMaterial({
+      color: 0xffffff,
+      transparent: true,
+      opacity: 0.5,
+    });
+    const lineSegments = new THREE.LineSegments(planeGeometry, lineMaterial);
+    lineSegments.rotation.x = -Math.PI / 2;
+    scene.add(lineSegments);
     // floor
     const plane = new THREE.Mesh(
-      new THREE.PlaneGeometry(10, 10),
-      new THREE.MeshStandardMaterial({ color: "#ffffff" })
+      planeGeometry,
+      new THREE.MeshStandardMaterial({
+        color: 0x156289,
+        emissive: 0x072534,
+        flatShading: true,
+        side: THREE.DoubleSide,
+        opacity: 0.5,
+        transparent: true,
+      })
     );
     plane.rotation.x = -Math.PI / 2;
     plane.receiveShadow = true;
@@ -83,7 +114,18 @@ const Physics: FunctionComponent = () => {
      */
     const world = new CANNON.World();
     world.gravity.set(0, -9.82, 0);
+    const defaultMaterial = new CANNON.Material("default");
+    const defaultContactMaterial = new CANNON.ContactMaterial(
+      defaultMaterial,
+      defaultMaterial,
+      {
+        friction: 0.1,
+        restitution: 0.7,
+      }
+    );
 
+    world.addContactMaterial(defaultContactMaterial);
+    world.defaultContactMaterial = defaultContactMaterial;
     const { sphereBody, sphereMesh } = createSphere(world);
     // 地板
     const groundBody = new CANNON.Body({
